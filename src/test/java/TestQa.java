d6fad49facf330c37a6d407a2e9c73284f0a3c2f
julia3010 <juliashka3010@mail.ru>
2025-05-12 01:20:47 +0300

    added junit examples

---
+++ src/test/java/TestQa.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+public class TestQa {
+
+    @Test
+    void example() {
+        System.out.println("huza!");
+    }
+
+    @Test
+    void example2() {
+        int a=2;
+        int b=3;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
